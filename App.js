662996d29cda359f7feed105c49f26f9420cfe84
Charles Cruse <charle9@vt.edu>
2023-08-14 21:21:16 -0400

    platform configuration

--- App.js
+++ App.js
@@ -21,67 +21,89 @@ import backgroundPhoto from "./app/assets/background.jpg";
 import ViewImageScreen from "./app/screens/ViewImageScreen.js";
 import AppText from "./app/components/AppText.js";
 
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+
 export default function App() {
-  // console.log("orientation is:", useDeviceOrientation());
-  // const orientation = useDeviceOrientation();
-  // console.log(orientation);
-
-  // const containerStyle = {
-  //   backgroundColor: "orange",
-  //   height: "50%",
-  //   width: orientation === "portrait" ? "100%" : "50%",
-  // };
-
-  // return (
-  //   <SafeAreaView style={styles.container}>
-  //     {/* <SafeAreaView style={[styles.container, containerStyle]}> */}
-  //     {/* <TouchableHighlight
-  //       onPress={() => {
-  //         console.log("image tapped");
-  //       }}
-  //     >
-  //       <Image
-  //         source={{
-  //           width: 200,
-  //           height: 300,
-  //           uri: "https://picsum.photos/200/300",
-  //         }}
-  //       />
-  //     </TouchableHighlight> */}
-
-  //     {/* <Button
-  //       title="click me"
-  //       // onPress={() =>
-  //       //   Alert.alert("Title of Alert", "Here is the message for the user", [
-  //       //     { text: "Yes", onPress: () => console.log("Yes") },
-  //       //     { text: "No" },
-  //       //   ])
-  //       // }
-  //       onPress={() => {
-  //         Alert.prompt("Title of Prompt", "Prompt Message", (text) =>
-  //           console.log(text)
-  //         );
-  //       }}
-  //       color="black"
-  //     /> */}
-
-  //     <Text>Hello there.</Text>
-  //     {/* <Image source={{ uri: backgroundPhoto, width: 200, height: 300 }} /> */}
-  //     <WelcomeScreen />
-  //   </SafeAreaView>
-  // );
-
-  // return <WelcomeScreen />;
-  // return <ViewImageScreen />;
-  return <AppText>I love React Native!</AppText>;
+  return (
+    <SafeAreaView>
+      <AppText>I love React Native</AppText>
+    </SafeAreaView>
+  );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
-  },
-});
+//How to import icons that you can manipulate without worrying about size/pixelation
+// export default function App() {
+//   return (
+//     <View>
+//       <MaterialCommunityIcons name="email" size={60} color="dodgerblue" />
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "#fff",
+//     alignItems: "center",
+//     justifyContent: "center",
+//     paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+//   },
+// });
+
+// export default function App() {
+//   // console.log("orientation is:", useDeviceOrientation());
+//   // const orientation = useDeviceOrientation();
+//   // console.log(orientation);
+
+//   // const containerStyle = {
+//   //   backgroundColor: "orange",
+//   //   height: "50%",
+//   //   width: orientation === "portrait" ? "100%" : "50%",
+//   // };
+
+//   // return (
+//   //   <SafeAreaView style={styles.container}>
+//   //     {/* <SafeAreaView style={[styles.container, containerStyle]}> */}
+//   //     {/* <TouchableHighlight
+//   //       onPress={() => {
+//   //         console.log("image tapped");
+//   //       }}
+//   //     >
+//   //       <Image
+//   //         source={{
+//   //           width: 200,
+//   //           height: 300,
+//   //           uri: "https://picsum.photos/200/300",
+//   //         }}
+//   //       />
+//   //     </TouchableHighlight> */}
+
+//   //     {/* <Button
+//   //       title="click me"
+//   //       // onPress={() =>
+//   //       //   Alert.alert("Title of Alert", "Here is the message for the user", [
+//   //       //     { text: "Yes", onPress: () => console.log("Yes") },
+//   //       //     { text: "No" },
+//   //       //   ])
+//   //       // }
+//   //       onPress={() => {
+//   //         Alert.prompt("Title of Prompt", "Prompt Message", (text) =>
+//   //           console.log(text)
+//   //         );
+//   //       }}
+//   //       color="black"
+//   //     /> */}
+
+//   //     <Text>Hello there.</Text>
+//   //     {/* <Image source={{ uri: backgroundPhoto, width: 200, height: 300 }} /> */}
+//   //     <WelcomeScreen />
+//   //   </SafeAreaView>
+//   // );
+
+//   // return <WelcomeScreen />;
+//   // return <ViewImageScreen />;
+//   // return <AppText>I love React Native!</AppText>;
+//   <View>
+//     <MaterialCommunityIcons name="email" size={60} />
+//   </View>;
+// }
--- app/components/AppText.js
+++ app/components/AppText.js
@@ -1,14 +1,36 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, Platform } from "react-native";
 
 function AppText({ children }) {
   return <Text style={styles.text}>{children}</Text>;
 }
+//Code below let's us specify what we want to do no each platform
+//We take this code here and put it into our stylesSheet and spread it
+// Platform.select({
+//     ios: {
+//         fontSize: 20,
+//         fontFamily: "Avenir",
+//     },
+//     android: {
+//         fontSize: 18,
+//         fontFamily: "Roboto",
+//     },
+// })
 
+//Below styles can actually be put into files specifying the platform which is easier again, as long as they are named the same, then don't have to list everything
 const styles = StyleSheet.create({
   text: {
-    fontSize: 18,
-    fontFamily: Platform.OS === "android" ? "Roboto" : "Avenir",
+    color: "tomato",
+    ...Platform.select({
+      ios: {
+        fontSize: 20,
+        fontFamily: "Avenir",
+      },
+      android: {
+        fontSize: 18,
+        fontFamily: "Roboto",
+      },
+    }),
   },
 });
 
