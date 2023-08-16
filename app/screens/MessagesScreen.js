5f728698314185c11c10e781e7458e703e921227
Charles Cruse <charle9@vt.edu>
2023-08-16 18:58:41 -0400

    added delete message functionality

--- app/screens/MessagesScreen.js
+++ app/screens/MessagesScreen.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { FlatList, StyleSheet, View } from "react-native";
 
 import ListItem from "../components/ListItem";
@@ -6,7 +6,7 @@ import Screen from "../components/Screen";
 import ListItemSeparator from "../components/ListItemSeparator";
 import ListItemDeleteAction from "../components/ListItemDeleteAction";
 
-const messages = [
+const initialMessages = [
   {
     id: 1,
     title: "T1",
@@ -22,6 +22,17 @@ const messages = [
 ];
 
 function MessagesScreen(props) {
+  const [messages, setMessages] = useState(initialMessages);
+
+  const handleDelete = (message) => {
+    //delete message from messages array
+    const newMessages = messages.filter(
+      (arrayMessage) => arrayMessage.id !== message.id
+    );
+    setMessages(newMessages);
+    //call server
+  };
+
   return (
     <Screen style={styles.screen}>
       <FlatList
@@ -33,7 +44,11 @@ function MessagesScreen(props) {
             subTitle={item.description}
             image={item.image}
             onPress={() => console.log("Message Selected", item)}
-            renderRightActions={ListItemDeleteAction}
+            // This syntax below is okay for just showing the component, but can't do an onPress for it, so need to change the syntax
+            // renderRightActions={ListItemDeleteAction}
+            renderRightActions={() => (
+              <ListItemDeleteAction onPress={() => handleDelete(item)} />
+            )}
           />
         )}
         ItemSeparatorComponent={ListItemSeparator}
